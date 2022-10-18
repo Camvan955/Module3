30bc513df214735e25b40adc50e4db760163c100
Camvan955 <nguyenvann955@gmail.com>
2022-10-18 22:46:05 +0700

    up

--- case_study/query.sql
+++ case_study/query.sql
@@ -18,18 +18,14 @@ group by khach_hang.ma_khach_hang
 order by count(hop_dong.ma_hop_dong);
 
 -- task 5:
-select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, (dv.chi_phi_thue +(dvdk.so_luong *dvdk.gia)) as tong_tien
+select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, (dv.chi_phi_thue +sum(dvdk.gia*hdct.so_luong)) as tong_tien
 from hop_dong hd
 join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
 join loai_khach lk on kh.ma_loai_khach = kh.ma_loai_khach
 join dich_vu dv on hd.ma_dich_vu = hd.ma_dich_vu
-right join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
-join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem;
-
-
-
-
-
+join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hd.ma_hop_dong;
 
 
 
